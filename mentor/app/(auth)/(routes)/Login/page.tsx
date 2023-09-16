256d919adda8a8310a9f9c168149b830e003724f
NizarAbiZaher <Nizabizaher@gmail.com>
2023-09-16 08:58:32 -0400

    login

---
+++ mentor/app/(auth)/(routes)/Login/page.tsx
@@ -0,0 +1,10 @@
+// Login page without the need for protection because users will need to access this page to login.
+const LoginPage = () => {
+    return (
+        <div>
+            Login Page (UnProtected)
+        </div>
+    )
+}
+
+export default LoginPage
--- mentor/app/page.tsx
+++
@@ -1,8 +0,0 @@
-// Home Page
-export default function Home() {
-  return (
-    <div className='text-green-400'>
-      Mentor AI
-    </div>
-  )
-}
